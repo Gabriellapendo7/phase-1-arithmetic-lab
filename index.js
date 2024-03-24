f0fc74e78abb11ae18753c91dec25302ba0f039a
gabriellapendo7 <gabriellapendo12@gmail.com>
2024-03-24 17:31:13 +0300

    changes

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-
 // Write your code here
+// Task 1
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+// Task 2
+let random = Math.floor(Math.random()*8)+3;
+
+// Task 3
+let num3 = 54;
+let num4 = 5;
+let mod = num3 % num4;
+
+// Task 4
+let max = Math.max(4,9,20);
+
+console.log("Result of multiplication:", multiply);
+console.log("Random integer greater than 0:", random);
+console.log("Remainder of num3 divided by num4:", mod);
+console.log("Highest number in the set:", max);
+
+
